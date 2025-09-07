b3df69f526e4d95cdcc3dc148ea9aad9fd05beec
joeruch23 <joeruch23@gmail.com>
2025-09-07 16:02:08 -0600

    Delete colorado-weather-network-no-ads directory

--- colorado-weather-network-no-ads/app/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function BlogPage() {
-  return (
-    <div className="card">
-      <h1 className="font-semibold mb-2">blog</h1>
-      <p className="text-sm">Write posts here. Later we can add a headless CMS.</p>
-    </div>
-  );
-}
